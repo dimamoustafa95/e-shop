4dae4a61478088b650e5c1be56dc2e33c4f6bf92
Unknown <unknown@example.com>
2022-01-16 14:37:10 +0200

    validation for online payment

--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -25,43 +25,53 @@
                         <div class="row checkout-form">
                             <div class="col-md-6">
                                 <label for="firstName">First Name</label>
-                                <input type="text" class="form-control" value="{{\Illuminate\Support\Facades\Auth::user()->name}}" name="f_name" placeholder="Enter First Name">
+                                <input type="text" class="form-control firstName" value="{{\Illuminate\Support\Facades\Auth::user()->name}}" name="f_name" placeholder="Enter First Name">
+                                <span id="fname_error" class="text-danger"></span>
                             </div>
                             <div class="col-md-6">
                                 <label for="lastName">Last Name</label>
-                                <input type="text" class="form-control" value="{{\Illuminate\Support\Facades\Auth::user()->l_name}}" name="l_name" placeholder="Enter Last Name">
+                                <input type="text" class="form-control lastName" value="{{\Illuminate\Support\Facades\Auth::user()->l_name}}" name="l_name" placeholder="Enter Last Name">
+                                <span id="lname_error" class="text-danger"></span>
                             </div>
                             <div class="col-md-6 mt-3">
                                 <label for="email">Email</label>
-                                <input type="text" class="form-control" value="{{\Illuminate\Support\Facades\Auth::user()->email}}" name="email" placeholder="Enter Email">
+                                <input type="text" class="form-control email" value="{{\Illuminate\Support\Facades\Auth::user()->email}}" name="email" placeholder="Enter Email">
+                                <span id="email_error" class="text-danger"></span>
                             </div>
                             <div class="col-md-6 mt-3">
                                 <label for="phoneNumber">Phone Number</label>
-                                <input type="text" class="form-control" value="{{\Illuminate\Support\Facades\Auth::user()->phone}}" name="phone" placeholder="Enter Phone Number">
+                                <input type="text" class="form-control phone" value="{{\Illuminate\Support\Facades\Auth::user()->phone}}" name="phone" placeholder="Enter Phone Number">
+                                <span id="phone_error" class="text-danger"></span>
                             </div>
                             <div class="col-md-6 mt-3">
                                 <label for="address1">Address 1</label>
-                                <input type="text" class="form-control" value="{{\Illuminate\Support\Facades\Auth::user()->address_1}}" name="address_1" placeholder="Enter Address 1">
+                                <input type="text" class="form-control address1" value="{{\Illuminate\Support\Facades\Auth::user()->address_1}}" name="address_1" placeholder="Enter Address 1">
+                                <span id="address1_error" class="text-danger"></span>
                             </div>
                             <div class="col-md-6 mt-3">
                                 <label for="address2">Address 2</label>
-                                <input type="text" class="form-control" value="{{\Illuminate\Support\Facades\Auth::user()->address_2}}" name="address_2" placeholder="Enter Address 2">
+                                <input type="text" class="form-control address2" value="{{\Illuminate\Support\Facades\Auth::user()->address_2}}" name="address_2" placeholder="Enter Address 2">
+                                <span id="address2_error" class="text-danger"></span>
                             </div>
                             <div class="col-md-6 mt-3">
                                 <label for="city">City</label>
-                                <input type="text" class="form-control" value="{{\Illuminate\Support\Facades\Auth::user()->city}}" name="city" placeholder="Enter City">
+                                <input type="text" class="form-control city" value="{{\Illuminate\Support\Facades\Auth::user()->city}}" name="city" placeholder="Enter City">
+                                <span id="city_error" class="text-danger"></span>
                             </div>
                             <div class="col-md-6 mt-3">
                                 <label for="state">State</label>
-                                <input type="text" class="form-control" value="{{\Illuminate\Support\Facades\Auth::user()->state}}" name="state" placeholder="Enter State">
+                                <input type="text" class="form-control state" value="{{\Illuminate\Support\Facades\Auth::user()->state}}" name="state" placeholder="Enter State">
+                                <span id="state_error" class="text-danger"></span>
                             </div>
                             <div class="col-md-6 mt-3">
                                 <label for="country">Country</label>
-                                <input type="text" class="form-control" value="{{\Illuminate\Support\Facades\Auth::user()->country}}" name="country" placeholder="Enter Country">
+                                <input type="text" class="form-control country"  value="{{\Illuminate\Support\Facades\Auth::user()->country}}" name="country" placeholder="Enter Country">
+                                <span id="country_error" class="text-danger"></span>
                             </div>
                             <div class="col-md-6 mt-3">
                                 <label for="pinCode">Pin Code</label>
-                                <input type="text" class="form-control" value="{{\Illuminate\Support\Facades\Auth::user()->pin_code}}" name="pin_code" placeholder="Enter Pin Code">
+                                <input type="text" class="form-control pincode" value="{{\Illuminate\Support\Facades\Auth::user()->pin_code}}" name="pin_code" placeholder="Enter Pin Code">
+                                <span id="pincode_error" class="text-danger"></span>
                             </div>
                         </div>
                     </div>
@@ -91,7 +101,8 @@
                             </tbody>
                         </table>
                         <hr>
-                        <button type="submit" class="btn btn-primary float-end">place order</button>
+                        <button type="submit" class="btn btn-success float-end w-100">place order | COD</button>
+                        <button type="button" class="btn btn-primary w-100 mt-3 razorpay-btn">Pay with Razorpay</button>
                     </div>
                 </div>
             </div>
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -99,6 +99,7 @@
 <script src="{{asset('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js')}}" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 <script src="{{asset('frontend/js/owl.carousel.min.js')}}"></script>
 <script src="{{asset('frontend/js/custom.js')}}"></script>
+<script src="{{asset('frontend/js/checkout.js')}}"></script>
 <script src="{{asset('https://unpkg.com/sweetalert/dist/sweetalert.min.js')}}"></script>
 @if(session('status'))
         <script>
